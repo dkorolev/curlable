832d7ab2968b8121ee355b4f151b8ad2cefbd49b
Dima Korolev <dmitry.korolev@gmail.com>
2017-10-08 11:27:03 -1000

    Multiline commands support.

--- curlable.js
+++ curlable.js
@@ -11,20 +11,31 @@ program
   .option('-c, --cmdline <cmdline>', 'The command to run.')
   .option('-p, --port <port>', 'Post to listen on.', parseInt)
   .option('-r, --route <route>', 'The route to serve the command on.')
+  .option('-q, --prompt <prompt>', 'The ready-to-process prompt, for multiline outputs.')
   .parse(process.argv);
 
 let cmdline = program.cmdline || 'bc -l'; // `bc -l` is a good default. -- D.K.
 let port = program.port || 8000;
 let route = program.route || '/';
+let options = {
+  prompt: program.prompt || null,
+};
 
-console.error('Making `' + cmdline + '` curlable.');
+process.stderr.write('Making `' + cmdline + '` curlable.\n');
+if (options.prompt) {
+  process.stderr.write('Multiline output mode, prompt: `' + options.prompt + '`.\n');
+}
 
-const instance = new curlable.curlable(cmdline, console.warn);
+const instance = new curlable.Curlable(cmdline, options, (msg) => { process.stderr.write(msg + '\n'); });
 
 curlable.readStreamByLines(process.stdin, (line) => {
-  instance.runQuery(line, (result) => {
-    console.log(result);
-  });
+  if (instance.ready) {
+    instance.runQuery(line, (result) => {
+      process.stdout.write(result);
+    });
+  } else {
+    process.stdout.write('Not yet available, waiting for the welcome prompt.\n');
+  }
 });
 
 var app = express();
@@ -33,10 +44,10 @@ app.use(bodyParser.text({
 }));
 
 instance.registerRoutes(app, route, port, () => {
-  console.error('Quitting the binary due to an extenal DELETE request.');
+  process.stderr.write('Quitting the binary due to an extenal DELETE request.\n');
   server.close();
   process.exit(0);
 });
 
 let server = app.listen(port);
-console.warn('Service started, listening on port ' + port + '.');
+process.stderr.write('Service started, listening on port ' + port + '.\n');
